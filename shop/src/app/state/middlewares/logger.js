597117f5716d5c6bdca1753a213203a74978a60c
Gediminas Luksas <gediminasluksas@gmail.com>
2019-03-20 20:55:15 +0200

    Actions
    Signed-off-by: Gediminas Luksas <gediminasluksas@gmail.com>

--- shop/src/app/state/middlewares/logger.js
+++ shop/src/app/state/middlewares/logger.js
@@ -1,13 +1,14 @@
 import shop from '../../../shop';
+
 const logger = ({ getState, dispatch }) => next => action => {
- // const state = getState();
+  const state = getState();
   const result = next(action);
-  //const nextState = getState();
+  const nextState = getState();
 
   if (action.type === shop.types.TOGGLE_FAVORITE_PRODUCT) {
     dispatch({ type: 'RANDOM' });
   }
-  //console.log(action.type, action.payload, 'state', state, result, nextState);
+  console.log(action.type, action.payload, 'state', state, result, nextState);
 
   return result;
 };
--- shop/src/auth/index.js
+++ shop/src/auth/index.js
@@ -2,5 +2,6 @@ import reducer from './reducer';
 import * as types from './actionTypes';
 import * as constants from './constants';
 import * as selector from './selector';
+import * as actions from './actions';
 
-export default { reducer, types, selector, constants };
+export default { reducer, types, selector, constants, actions };
